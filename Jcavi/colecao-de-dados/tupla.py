cf43b9c61513fc48fc330df7693fa4f659e6425b
PhilippeRMS <philipperms@gmail.com>
2022-03-20 22:45:43 -0300

    Revisando tuplas

---
+++ Jcavi/colecao-de-dados/tupla.py
@@ -0,0 +1,21 @@
+# Tupla é uma lista imutavel.
+## A tupla nao permite adiçao e remoção 
+###A tupla é separada por virgula
+#### Possui indice de memoria
+
+tupla_numeros = (43,78,12,89)
+tupla_numeros1 = 56,
+tupla_numeros2 = 31
+
+tupla_carros = 'Gol','Fiesta','Sandeiro'
+
+# Dentro de outra estrutura de dados as tuplas recebem parenteses  ()
+
+lista1 = [(1,3),(2,4),(1,2)]
+
+lista1.sort()
+
+print(lista1)
+
+print(lista1[0])
+
